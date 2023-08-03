bf2c7c801595138c8855e455ac2854eec204a129
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-08-03 22:55:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// console.log("-".repeat(70))
+// console.log(5 + 3)
+// console.log("_".repeat(70))
+/*console.log(16 - 8)
+console.log("=".repeat(70))
+console.log(4 * 2)
+console.log("_".repeat(70))*/
+console.log(16 / 2)
+console.log("-".repeat(70))
